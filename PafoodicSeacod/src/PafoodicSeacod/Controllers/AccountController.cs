42c15c15960ba6b31da69bb12f9aa53088aaa158
Andrew Glines <aglines-epicodus@gmail.com>
2017-08-21 19:44:30 -0700

    Registration link

---
+++ PafoodicSeacod/src/PafoodicSeacod/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using PafoodicSeacod.Models;
+
+
+namespace PafoodicSeacod.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly AppDbContext _db;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController (UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
