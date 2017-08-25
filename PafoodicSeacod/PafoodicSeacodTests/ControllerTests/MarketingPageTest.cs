47665f078905afe9a2f7614fa8e71e3c4eab913b
Andrew Glines <aglines-epicodus@gmail.com>
2017-08-25 11:54:02 -0700

    Marketing page tests and controllers

---
+++ PafoodicSeacod/PafoodicSeacodTests/ControllerTests/MarketingPageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PafoodicSeacod.Models;
+using Xunit;
+using PafoodicSeacod.Controllers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PafoodicSeacod.Tests.ControllerTests
+{
+    public class MarketingControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Marketing()
+        {
+            //Arrange
+            MarketingController controller = new MarketingController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
---
+++ PafoodicSeacod/PafoodicSeacodTests/ModelTests/ModelTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PafoodicSeacod.Models;
+using Xunit;
+
+namespace PafoodicSeacod.Tests
+{
+    public class ModelTest
+    {
+           {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var Subscriber = new Subscriber();
+            Subscriber.Email = "1@1.1";
+
+            //Act
+            var result = Subscriber.Email;
+
+            //Assert
+            Assert.Equal("1@1.1", result);
+        }
+    }
+}
+
+
